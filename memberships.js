3aa00de903fa1a6472cd576644e48a1893049c93
nteinert2005 <nteinert2005@gmail.com>
2020-09-01 10:02:46 -0500

    Update memberships.js

--- memberships.js
+++ memberships.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $('#enrollnow').click(function(){
-    alert('hi');
-    console.log('script executed!');
+    console.log('script ran from github && executed!');
   });
 });
